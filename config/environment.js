18ad1806e15effa394bf57e128ac84e269a7714b
Susan Puckett <sepuckett86@gmail.com>
2019-11-23 09:31:41 -0800

    move forward in tutorial with Dan

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'paste your Mapbox access token here';
+
   return ENV;
 };
