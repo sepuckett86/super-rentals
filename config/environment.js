2dac1ecdd7aab03bfcae8a77b65d3afd5c05ecab
Susan Puckett <sepuckett86@gmail.com>
2019-11-25 17:01:45 -0800

    get to twitter button in tutorial

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
+  if(environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -31,7 +31,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
-  if (environment === 'test') {
+  if(environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
 
@@ -43,7 +43,7 @@ module.exports = function(environment) {
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
+  if(environment === 'production') {
     // here you can enable a production-specific feature
   }
 
